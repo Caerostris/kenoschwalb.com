9f39f525931d8ede09582d622b03515b96189fba
Keno Schwalb <caerostris@gmail.com>
2018-02-28 05:20:35 +0000

    chore: fix compressed CSS build

--- build.sh
+++ build.sh
@@ -4,5 +4,12 @@ rm -rf build
 mkdir build
 cp index.html build/
 cp -R static build && rm build/static/css/*.css
-cat static/css/*.css | node_modules/csso-cli/bin/csso > build/static/css/main.min.css
+cat \
+	static/css/main.css \
+	static/css/layout.css \
+	static/css/home.css \
+	static/css/fencer.css \
+	static/css/photographer.css \
+	static/css/student.css \
+	| node_modules/csso-cli/bin/csso > build/static/css/main.min.css
 perl -i -0pe "s/(\s*<link rel='stylesheet' [^>]+>\n)+/\n\t\t<link rel='stylesheet' type='text\/css' href='static\/css\/main.min.css' \/>\n/" build/index.html
