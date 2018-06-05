f9d500eab21107637d367ea241e76500cc1a4d45
Keno Schwalb <caerostris@gmail.com>
2018-06-05 12:46:46 +0200

    feat: download instagram images during build for CDN delivery

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 rm -rf build
 mkdir build
@@ -13,3 +14,6 @@ cat \
 	static/css/student.css \
 	| node_modules/csso-cli/bin/csso > build/static/css/main.min.css
 perl -i -0pe "s/(\s*<link rel='stylesheet' [^>]+>\n)+/\n\t\t<link rel='stylesheet' type='text\/css' href='static\/css\/main.min.css' \/>\n/" build/index.html
+mkdir build/static/images/thumbnails
+cd build
+node ../precache_images.js index.html static/images/thumbnails "https://www.instagram.com/p/([^/]+)/media"
---
+++ precache_images.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('follow-redirects').https;
+const cheerio = require('cheerio');
+
+if (process.argv.length < 5) {
+	console.error(`Usage: ${process.argv[0]} input_file output_file image_folder [url selectors ...]`);
+	process.exit(1);
+}
+
+const urlSelectors = process.argv.slice(4);
+const inputFile = process.argv[2];
+const outputFolder = process.argv[3];
+const htmlContents = fs.readFileSync(inputFile, 'utf-8');
+const $ = cheerio.load(htmlContents);
+
+$('.tile.image').each(function() {
+	const tile = $(this);
+	const backgroundImage = tile.css('background-image');
+	const matches = backgroundImage.match(/url\(([^\)]+)\)/);
+	if (matches.length != 2) {
+		return;
+	}
+
+	const url = matches[1];
+	for (selectorId in urlSelectors) {
+		const selector = urlSelectors[selectorId];
+		const selectorMatches = url.match(selector);
+		if (selectorMatches.length != 2) {
+			return;
+		}
+
+		const fileId = selectorMatches[1];
+		const outputFile = path.join(outputFolder, fileId);
+		const outStream = fs.createWriteStream(outputFile);
+
+		// rewrite to cached file in HTML tree
+		tile.css('background-image', `url(${outputFile})`);
+
+		// cache file at given url
+		https.get(url, res => {
+			console.log(res.statusCode);
+			res.pipe(outStream);
+			outStream.on('finish', () => outStream.close(() => {
+				console.log(`Downloaded ${fileId}`);
+			}));
+		}).on('error', err => {
+			console.error('Download error:');
+			console.error(err);
+			process.exit(1);
+		});
+	}
+});
+
+fs.writeFileSync(inputFile, $.html(), 'utf-8');
